179b57bcd4b3ba3cb7750eeb49b6dc25725bf270
Rishu Sharma <rishushar99@gmail.com>
2025-09-04 17:07:14 -0400

    updated nav bar

--- app/components/navBar.tsx
+++ app/components/navBar.tsx
@@ -1,35 +1,28 @@
-import React from 'react'
-import ShimmerButton from './subcomponents/shimmerTile'
+import React from "react";
+import ShimmerButton from "./subcomponents/shimmerTile";
 
 const NavBar: React.FC = () => {
   return (
-    <header className='fixed inset-x-0 top-0 h-20 bg-gray-800/90 backdrop-blur z-50 flex items-center justify-between px-6'>
+    <header className="fixed inset-x-0 top-0 h-20 bg-gray-800/90 backdrop-blur z-50 flex items-center justify-between px-6">
       <img
-        className='h-12'
-        src='https://github.com/Firebolt9907/firebolt9907.github.io/blob/react-refactor/assets/myPics/signature.webp?raw=true'
-        alt='My Signature'
+        className="h-12"
+        src="https://github.com/Firebolt9907/firebolt9907.github.io/blob/react-refactor/assets/myPics/signature.webp?raw=true"
+        alt="My Signature"
         onClick={() => {
-          window.location.href = ''
+          window.location.href = "https://rishitsharma.netlify.app";
         }}
       />
-      <nav className='flex row'>
+      <nav className="flex row">
         <ShimmerButton
-          content={<h3>Projects</h3>}
+          content={<h3>Contact Me</h3>}
           tile={false}
           handleClick={() => {
-            window.location.href = '#projects'
-          }}
-        />
-        <ShimmerButton
-          content={<h3>Contact</h3>}
-          tile={false}
-          handleClick={() => {
-            window.location.href = '#contact'
+            window.location.href = "mailto:rishushar99@gmail.com";
           }}
         />
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
